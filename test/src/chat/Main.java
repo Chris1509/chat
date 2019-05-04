b1a055111294a0ce2491a59939910cf97e078ce9
Chris <Chris@DESKTOP-EOKH2OA>
2019-05-04 16:39:59 +0200

    weniger Fehler

--- test/src/chat/Main.java
+++ test/src/chat/Main.java
@@ -1,17 +1,5 @@
 package chat;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.net.InetAddress;
-import java.net.InetSocketAddress;
-import java.net.Socket;
-import java.net.SocketAddress;
-import java.net.SocketTimeoutException;
 
-import java.time.ZonedDateTime;
-import java.time.format.DateTimeFormatter;
 
 public class Main {
 
@@ -26,7 +14,5 @@ public class Main {
 		t1.start();
 		t2.start();
 	}
-	
-	SimpleSocketClient()
-	  {
-	    
+
+}
--- test/src/chat/SimpleSocketClient.java
+++ test/src/chat/SimpleSocketClient.java
@@ -14,125 +14,101 @@ import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class SimpleSocketClient {
-	
+
 	String testServerName = "localhost";
-    int port = 33333;
-    try
-    {
-	
-      Socket socket = openSocket(testServerName, port);
-	
-      String start = writeToAndReadFromSocket(socket, "dslp/1.2\r\nrequest time\r\ndslp/end\r\n");
-      String[] abc = start.split("\r\n");
-      for(int i=0; i<abc.length; i++){
-        if(abc[i].contains("dslp/1.2")){
-			
-          System.out.println("Verbunden zu " + socket.getInetAddress() +":"+ socket.getPort());
-        }
-        if(abc[i].contains("response time")){
-        String input = abc[i+1];
-		ZonedDateTime z = ZonedDateTime.parse(input, DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssXXX"));
-		String output = z.format(DateTimeFormatter.ofPattern("EEE MMM dd HH:mm:ss z yyyy"));
-        System.out.println("Aktuelle Zeit auf dem Server: " + output);
-        }
-        if(abc[i].contains("dslp/end")) {
-          break;
-        }
+	int port = 33333;
+	{
 
-      }
+		try {
 
-    //  System.out.println(start);
-   //  System.out.println(result);
-   //  System.out.println(end);
-      // close the socket, and we're done
-      socket.close();
-    }
-    catch (Exception e)
-    {
-      e.printStackTrace();
-    }
-  }
+			Socket socket = openSocket(testServerName, port);
 
-  private String writeToAndReadFromSocket(Socket socket, String writeTo) throws Exception
-  {
-    try
-    {
-      // write text to the socket
-      BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-      bufferedWriter.write(writeTo);
-      bufferedWriter.flush();
+			String start = writeToAndReadFromSocket(socket, "dslp/1.2\r\nrequest time\r\ndslp/end\r\n");
+			String[] abc = start.split("\r\n");
+			for (int i = 0; i < abc.length; i++) {
+				if (abc[i].contains("response time")) {
 
-      // read text from the socket
-      BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-      String str;
-      StringBuilder sb = new StringBuilder();
-      while ((str = bufferedReader.readLine()) != null)
-      {
-        sb.append(str + "\r\n");
+				}
+				if (abc[i].contains("dslp/end")) {
+					break;
+				}
 
-       if(str.contains("dslp/1.2")){
+			}
 
-        }
+			// System.out.println(start);
+			// System.out.println(result);
+			// System.out.println(end);
+			// close the socket, and we're done
+			socket.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
-        if(str.contains("")){
+	private String writeToAndReadFromSocket(Socket socket, String writeTo) throws Exception {
+		try {
+			// write text to the socket
+			BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+			bufferedWriter.write(writeTo);
+			bufferedWriter.flush();
 
-        }
+			// read text from the socket
+			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			String str;
+			StringBuilder sb = new StringBuilder();
+			while ((str = bufferedReader.readLine()) != null) {
+				sb.append(str + "\r\n");
 
-        if(str.contains("/end")){
-            break;
+				if (str.contains("dslp/1.2")) {
 
-        }
-      }
+				}
 
-      // close the reader, and return the results as a String
-      bufferedReader.close();
+				if (str.contains("")) {
 
-      return sb.toString();
-    }
-    catch (IOException e)
-    {
-      e.printStackTrace();
-      throw e;
-    }
-  }
+				}
 
-  /**
-   * Open a socket connection to the given server on the given port.
-   * This method currently sets the socket timeout value to 10 seconds.
-   * (A second version of this method could allow the user to specify this timeout.)
-   */
-  private Socket openSocket(String server, int port) throws Exception
-  {
-    Socket socket;
+				if (str.contains("/end")) {
+					break;
 
+				}
+			}
 
-    // create a socket with a timeout
-    try
-    {
-      InetAddress inteAddress = InetAddress.getByName(server);
-      SocketAddress socketAddress = new InetSocketAddress(inteAddress, port);
+			// close the reader, and return the results as a String
+			bufferedReader.close();
 
-      // create a socket
-      socket = new Socket();
+			return sb.toString();
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw e;
+		}
+	}
 
-      // this method will block no more than timeout ms.
-      int timeoutInMs = 10*1000;   // 10 seconds
-      socket.connect(socketAddress, timeoutInMs);
+	/**
+	 * Open a socket connection to the given server on the given port. This method
+	 * currently sets the socket timeout value to 10 seconds. (A second version of
+	 * this method could allow the user to specify this timeout.)
+	 */
+	private Socket openSocket(String server, int port) throws Exception {
+		Socket socket;
 
-      return socket;
-    }
-    catch (SocketTimeoutException ste)
-    {
-      System.err.println("Timed out waiting for the socket.");
-      ste.printStackTrace();
-      throw ste;
-    }
-  }
+		// create a socket with a timeout
+		try {
+			InetAddress inteAddress = InetAddress.getByName(server);
+			SocketAddress socketAddress = new InetSocketAddress(inteAddress, port);
 
-}
+			// create a socket
+			socket = new Socket();
 
+			// this method will block no more than timeout ms.
+			int timeoutInMs = 10 * 1000; // 10 seconds
+			socket.connect(socketAddress, timeoutInMs);
 
-}
-
+			return socket;
+		} catch (SocketTimeoutException ste) {
+			System.err.println("Timed out waiting for the socket.");
+			ste.printStackTrace();
+			throw ste;
+		}
+	}
 
 }
